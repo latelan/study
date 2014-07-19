1a5a74f2b39da8888418475987dd2c3804040c7f
amta <amta@amta-G31M-ES2C>
2014-07-19 08:06:10 +0800

    练习Linux C 编程实战 线程控制 创建线程

---
+++ unix_systems_programming/bufferout.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	fprintf(stdout,"a");
+	fprintf(stdout,"a has been written\n");
+	fprintf(stdout,"b");
+	fprintf(stdout,"b has been written\n");
+	fprintf(stdout,"\n");
+
+	return 0;
+}
---
+++ unix_systems_programming/chapter_8/mysignal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <signal.h>
+
+void handler_sigint(int signo) {
+	printf("recv SIGINT\n");
+}
+
+int main(void) {
+	signal(SIGINT,handler_sigint);
+
+	while(1);
+
+	return 0;
+}
