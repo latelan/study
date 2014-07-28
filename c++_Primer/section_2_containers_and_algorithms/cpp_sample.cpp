0e573aaa2f12b35996539428f1cc392a21c72652
latelan <coolboy1353@163.com>
2014-07-29 07:58:01 +0800

    c++程序模板

---
+++ c++_Primer/section_2_containers_and_algorithms/cpp_sample.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: cpp_sample.cpp
+	> Author: latelan, coolboy1353@163.com
+	> Created Time: 2014-07-29 07:53:39
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void)
+{
+	string s;
+	cin >> s;
+	cout << s << endl;
+
+	return 0;
+}
