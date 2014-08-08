09a8f10fef70c9499111d2ecf7536e1e99eb9810
latelan <coolboy1353@163.com>
2014-08-08 09:53:45 +0800

    finsh fcntl

--- linux_c_programming/chapter_6_file/fcntl_access.c
+++ linux_c_programming/chapter_6_file/fcntl_access.c
@@ -5,6 +5,7 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -24,7 +25,7 @@ int main()
 	int access_mode;
 	int fd;
 
-	if((fd = open("example_64",O_CREAT | O_TRUNC | O_RWXU )) == -1 ) {
+	if((fd = open("example_64",O_CREAT | O_TRUNC | O_RDWR, S_IRWXU )) == -1 ) {
 		my_err("open",__LINE__);
 	}
 
@@ -34,12 +35,29 @@ int main()
 	}
 
 	/* 获取文件打开方式 */
-	if((ret  fcntl(fd,F_GETFL,0)) < 0) {
+	if((ret = fcntl(fd,F_GETFL,0)) < 0) {
 		my_err("fcntl",__LINE__);
 	}
 
-	access_mod = ret & O_ACCMODE;
+	access_mode = ret & O_ACCMODE;
 	if(access_mode == O_RDONLY) {
-
+		printf("example_64 access mode: read only");
+	} else if (access_mode == O_WRONLY) {
+		printf("example_64 access mode: write only");
+	} else if (access_mode == O_RDWR) {
+		printf("example_64 access mode: read + write");
 	}
+
+	if (ret & O_APPEND) {
+		printf(" ,append");
+	}
+	if (ret * O_NONBLOCK) {
+		printf(" ,nonblock");
+	}
+	if (ret & O_SYNC) {
+		printf(" ,sync");
+	}
+	printf("\n");
+
+	return 0;
 }
